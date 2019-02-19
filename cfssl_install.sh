ec881c5c7037d46036ddf7c4439a3257d4636cf7
Destiny <569964924@qq.com>
2019-02-19 16:56:14 +0800

    ff

--- cfssl_install.sh
+++ cfssl_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo curl -s -L -o /usr/sbin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 
-sudo curl -s -L -o /usr/sbin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 
-sudo curl -s -L -o /usr/sbin/cfssl-certinfo https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64 
+sudo curl  -o /usr/sbin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 
+sudo curl  -o /usr/sbin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 
+sudo curl  -o /usr/sbin/cfssl-certinfo https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64 
 sudo chmod +x /bin/cfssl*
